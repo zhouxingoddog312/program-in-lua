2a0ac736dae064026a121508d244ec56993d8367
zhou xin <zhouxin312@qq.com>
2021-01-10 10:45:30 +0800

    chapter7 complete

--- chapter_7/exercise/7-5.lua
+++ chapter_7/exercise/7-5.lua
@@ -1,8 +1,9 @@
 #!/usr/bin/lua
+-- 使用第二个参数表示要输出的行数
 local offset=0
 local count=0
 file=io.open(arg[1])
-while count~=2 do
+while count~=arg[2]+1 do
 	offset=offset+1
 	flag=file:seek("end",-offset)
 	if not flag then
