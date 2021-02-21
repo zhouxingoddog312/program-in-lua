9a12e5e57f378505440b80e7a161d3ba99e67e4e
zhou xin <zhouxin312@qq.com>
2021-02-21 20:51:09 +0800

    for a rest

--- chapter_15/exercise/15-2.lua
+++ chapter_15/exercise/15-2.lua
@@ -3,20 +3,18 @@ function basicSerialize(o)
 	return string.format("%q",o)
 end
 function serialize(o,deep)
-	deep=deep or 0
+	local deep=deep or 0
 	local t=type(o)
 	if t=="boolean" or t=="number" or t=="string" or t=="nil" then
 		io.write(string.format("%q",o))
 	elseif t=="table" then
-		deep=deep+1
 		io.write("{\n")
 		for k,v in pairs(o) do
---			io.write(" ",k," = ")
-			io.write(string.rep("\t",deep),string.format(" [%s] = ",basicSerialize(k)))
-			serialize(v,deep)
+			io.write(string.rep("\t",deep+1),string.format(" [%s] = ",basicSerialize(k)))
+			serialize(v,deep+1)
 			io.write(",\n")
 		end
-		io.write("}\n")
+		io.write(string.rep("\t",deep),"}\n")
 	else
 		error("cannot serialize a "..type(o))
 	end
--- chapter_15/exercise/15-3.lua
+++ chapter_15/exercise/15-3.lua
@@ -1,26 +1,26 @@
 #!/usr/bin/lua
+reserved={["and"]=true,["break"]=true,["do"]=true,["else"]=true,["elseif"]=true,["end"]=true,["false"]=true,["goto"]=true,["for"]=true,["function"]=true,["if"]=true,["in"]=true,["local"]=true,["nil"]=true,["not"]=true,["or"]=true,["repeat"]=true,["return"]=true,["then"]=true,["true"]=true,["until"]=true,["while"]=true}
 function basicSerialize(o)
 	return string.format("%q",o)
 end
 function serialize(o,deep)
-	deep=deep or 0
+	local deep=deep or 0
 	local t=type(o)
 	if t=="boolean" or t=="number" or t=="string" or t=="nil" then
 		io.write(string.format("%q",o))
 	elseif t=="table" then
-		deep=deep+1
 		io.write("{\n")
 		for k,v in pairs(o) do
 			local s=string.match(k,"^[%a_]+[%w_]*$")
-			if s then
+			if s and not reserved[s] then
 				io.write(string.rep("\t",deep)," ",k," = ")
 			else
 				io.write(string.rep("\t",deep),string.format(" [%s] = ",basicSerialize(k)))
 			end
-			serialize(v,deep)
+			serialize(v,deep+1)
 			io.write(",\n")
 		end
-		io.write("}\n")
+		io.write(string.rep("\t",deep),"}\n")
 	else
 		error("cannot serialize a "..type(o))
 	end
