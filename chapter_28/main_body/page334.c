d8aa92b983b09e6290e011cc6af17b714af343c0
zhouxin <zhouxin312@qq.com>
2021-04-08 23:40:05 +0800

    mainbody

---
+++ chapter_28/main_body/page334.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#define MAX_COLOR 255
+
+struct ColorTable
+{
+	char *name;
+	unsigned char red,green,blue;
+}colortable[]=
+{
+	{"WHITE",MAX_COLOR,MAX_COLOR,MAX_COLOR},
+	{"RED",MAX_COLOR,0,0},
+	{"GREEN",0,MAX_COLOR,0},
+	{"BLUE",0,0,MAX_COLOR},
+	{NULL,0,0,0}
+};
+void setcolorfield(lua_State *L,const char *index,int value)	//假定表位于栈顶
+{
+	lua_pushnumber(L,(double)value/MAX_COLOR);
+	lua_setfield(L,-2,index);
+}
+void setcolor(lua_State *L,struct ColorTable *ct)
+{
+	lua_createtable(L,0,3);
+	setcolorfield(L,"red",ct->red);
+	setcolorfield(L,"green",ct->green);
+	setcolorfield(L,"blue",ct->blue);
+	lua_setglobal(L,ct->name);
+}
+
+void error(lua_State *L,const char *fmt,...)
+{
+	va_list argp;
+	va_start(argp,fmt);
+	vfprintf(stderr,fmt,argp);
+	va_end(argp);
+	lua_close(L);
+	exit(EXIT_FAILURE);
+}
+
+int getglobint(lua_State *L,const char *var)
+{
+	int isnum,result;
+	lua_getglobal(L,var);
+	result=(int)lua_tointegerx(L,-1,&isnum);
+	if(!isnum)
+		error(L,"'%s' should be a number\n",var);
+	lua_pop(L,1);
+	return result;
+}
+void load(lua_State *L,const char *fname,int *w,int *h)
+{
+	if(luaL_loadfile(L,fname)||lua_pcall(L,0,0,0))
+		error(L,"can not run config. file:%s",lua_tostring(L,-1));
+	*w=getglobint(L,"width");
+	*h=getglobint(L,"height");
+}
+int getcolorfield(lua_State *L,const char *key)		//此处假定表已经位于栈顶
+{
+	int result;
+	if(lua_getfield(L,-1,key)!=LUA_TNUMBER)
+		error(L,"invalid component '%s' in color",key);
+	result=(int)(lua_tonumberx(L,-1,NULL)*MAX_COLOR);
+	lua_pop(L,1);
+	return result;
+}
+int main(void)
+{
+	lua_State *L=luaL_newstate();
+	luaL_openlibs(L);
+	int width,height,red,green,blue;
+
+/*	用于注册颜色
+	int i=0;
+	while(colortable[i].name!=NULL)
+		setcolor(L,&colortable[i++]);
+*/
+
+	load(L,"config",&width,&height);
+	printf("width=%d	height=%d\n",width,height);
+
+	lua_getglobal(L,"background");
+/*
+	if(!lua_istable(L,-1))
+		error(L,"'background' is not a table");
+	red=getcolorfield(L,"red");
+	green=getcolorfield(L,"green");
+	blue=getcolorfield(L,"blue");
+*/
+	if(lua_isstring(L,-1))
+	{
+		const char *colorname=lua_tostring(L,-1);
+		int i;
+		for(i=0;colortable[i].name!=NULL;i++)
+		{
+			if(strcmp(colorname,colortable[i].name)==0)
+				break;
+		}
+		if(colorname==NULL)
+			error(L,"invalid color name (%s)",colorname);
+		else
+		{
+			red=colortable[i].red;
+			green=colortable[i].green;
+			blue=colortable[i].blue;
+		}
+	}
+	else if(lua_istable(L,-1))
+	{
+		red=getcolorfield(L,"red");
+		green=getcolorfield(L,"green");
+		blue=getcolorfield(L,"blue");
+	}
+	else
+		error(L,"invalid value for 'background'");
+
+	printf("red=%d		green=%d	blue=%d\n",red,green,blue);
+	lua_close(L);
+	return 0;
+}
+
+
+
